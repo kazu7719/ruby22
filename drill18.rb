527d3acc88e7f074ca457a11e9d82fbb4c858b46
kazu7719 <kazuki.eripon7719@icloud.com>
2025-11-15 14:43:08 +0900

    ruby ドリル問題18 2週目

---
+++ drill18.rb
@@ -0,0 +1,14 @@
+def police_trouble(a, b)
+  if a && b
+    puts "True"
+  elsif !a && !b
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+# 呼び出し例
+police_trouble(true, true) 
+police_trouble(false, false)
+police_trouble(true, false) 
