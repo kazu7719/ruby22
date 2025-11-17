839f326cd9c8352e3ba143b197ffa34c70c28979
kazu7719 <kazuki.eripon7719@icloud.com>
2025-11-17 10:06:11 +0900

    ruby ドリル問題24 2週目

---
+++ drill24.rb
@@ -0,0 +1,12 @@
+def sleep_in(is_weekday, is_vacation)
+  if is_vacation
+    puts "true"
+  elsif ! is_weekday && ! is_vacation
+    puts "true"
+  else
+    puts "false"
+  end
+end
+
+# 呼び出し例
+sleep_in(false, true)
