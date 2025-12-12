cbbe5879309c812b9f346c4e9f13a8c0c1b0eabc
kazu7719 <kazuki.eripon7719@icloud.com>
2025-12-12 16:28:20 +0900

    ruby ドリル問題36 2週目

---
+++ drill36.rb
@@ -0,0 +1,13 @@
+def withdraw(balance, amount)
+  fee = 110 # 手数料
+ if balance >= (amount + fee)
+   puts "#{amount}円引き落としました。残高は#{balance - amount - fee}円です"
+ else
+   puts "残高不足です"
+ end
+end
+
+balance = 100000 # 残高
+puts "いくら引き落としますか？（手数料110円かかります）"
+money = gets.to_i
+withdraw(balance, money)
