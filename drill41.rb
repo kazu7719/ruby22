99791e5388bd18322b9cc35598b98a5c47caa846
kazu7719 <kazuki.eripon7719@icloud.com>
2026-01-06 11:09:38 +0900

    ruby ドリル問題41 2週目

---
+++ drill41.rb
@@ -0,0 +1,15 @@
+def close_far(a,b,c)
+  x = (a - b).abs
+  y = (a - c).abs
+  z = (b - c).abs
+
+  if x == 1  && z >= 2
+    puts "True"
+  elsif y == 1 && z >= 2
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+close_far(4,1,3)
