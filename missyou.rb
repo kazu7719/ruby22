5bbeed77a3cabf39dd4bab09387d85db89ab3539
kazu7719 <kazuki.eripon7719@icloud.com>
2026-02-01 09:52:00 +0900

    Update missyou.rb

--- missyou.rb
+++ missyou.rb
@@ -1,3 +1,5 @@
-name = "テックキャンプ"
+tax = 0.1
+pen_price = 100
+book_price = 3000
 
-puts "私の名前は#{name}です。"
+puts "#{pen_price}円のペンの消費税は#{pen_price * tax}円、#{book_price}円の本の消費税は#{book_price * tax}円です。"
