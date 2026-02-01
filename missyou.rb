38c53ae53be3886c7cbc4cb698ea57823fb7533f
kazu7719 <kazuki.eripon7719@icloud.com>
2026-02-01 09:44:51 +0900

    Update missyou.rb

--- missyou.rb
+++ missyou.rb
@@ -1,13 +1,1 @@
-class Student
-  def set_name(name)
-    @name = name
-  end
-
-  def initialize.introduce
-    puts "私の名前は#{@name}です。"
-  end
-end
-
-student = Student.new
-student.set_name("山田太郎")
-student.introduce
+puts "1日は#{24 * 60}分です"
