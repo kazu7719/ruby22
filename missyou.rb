b1e4a2a8b2c0d83af27bd1b1da5cd2f05567adb6
kazu7719 <kazuki.eripon7719@icloud.com>
2026-02-02 17:49:59 +0900

    Update missyou.rb

--- missyou.rb
+++ missyou.rb
@@ -1,9 +1,5 @@
-input = gets.to_i
+ 10.times do |i|
+   puts "#{i + 1}回目の繰り返し"
+ end
 
-if input <= 0
-  puts "0以下の数字です"
-elsif input >= 10
-  puts "10以上の数字です"
-else
-  puts "0より大きく10より小さい数字です"
-end
+ 
