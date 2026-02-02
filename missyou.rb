a6b1b7c4b900e5421c296fde45fb689cc4c140f4
kazu7719 <kazuki.eripon7719@icloud.com>
2026-02-02 17:42:56 +0900

    Update missyou.rb

--- missyou.rb
+++ missyou.rb
@@ -1,7 +1,9 @@
-user = {name: "山田", age:20}
+input = gets.to_i
 
-
-user[:age] = 22
-user[:gender] = "男性"
-
-puts user
+if input <= 0
+  puts "0以下の数字です"
+elsif input >= 10
+  puts "10以上の数字です"
+else
+  puts "0より大きく10より小さい数字です"
+end
