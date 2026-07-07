9d195ecf83890b8fcd25d57ad6b10390a2404e8b
kazu7719 <kazuki.eripon7719@icloud.com>
2026-07-07 11:31:09 +0900

    rubyの基礎②

--- again.rb
+++ again.rb
@@ -1,2 +1,6 @@
-# 「put」になってしまっている
-put "Hello World"
+japanese = 80
+english = 50
+math = 70
+average = (japanese + english + math) / 3
+
+puts "3教科の平均点は、#{average}点です。"
--- seconds.rb
+++ seconds.rb
@@ -1,8 +1,8 @@
-puts "ようこそ！\n名前を入力してください"
-name = gets.chomp
+"ようこそ！名前を入力してください"
+name = "タロウ"
 
-puts "秒数を知りたい時間を分単位で入力してください"
-minutes = gets.to_i
+"秒数を知りたい時間を分単位で入力してください"
+minutes = 5
 seconds = minutes * 60
 
-puts "#{name}さん、#{minutes}分は#{seconds}秒です！"
+"#{name}さん、#{minutes}分は#{seconds}秒です！"
