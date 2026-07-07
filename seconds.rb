6555896b4db35ab94400cbef499e76666f0d01b0
kazu7719 <kazuki.eripon7719@icloud.com>
2026-07-07 11:42:53 +0900

    rubyの基礎③

--- seconds.rb
+++ seconds.rb
@@ -1,8 +1,8 @@
-"ようこそ！名前を入力してください"
+puts "ようこそ！\n名前を入力してください"
 name = "タロウ"
 
-"秒数を知りたい時間を分単位で入力してください"
+puts "秒数を知りたい時間を分単位で入力してください"
 minutes = 5
 seconds = minutes * 60
 
-"#{name}さん、#{minutes}分は#{seconds}秒です！"
+puts "#{name}さん、#{minutes}分は#{seconds}秒です！"
