aeb2959e89f2692e700cc8e6f3e396bc7aac328d
kazu7719 <kazuki.eripon7719@icloud.com>
2026-07-08 11:10:16 +0900

    rubyの基礎⑧

--- again.rb
+++ again.rb
@@ -1,9 +1,7 @@
-input = gets.to_i
+sum = 0
 
-if input <= 0
-  puts "0以下の数字です"
-elsif input >= 10
-  puts "10以上の数字です"
-else
-  puts "0より大きく10より小さい数字です"
+10.times do |i|
+  sum = sum + i + 1
 end
+
+puts sum
